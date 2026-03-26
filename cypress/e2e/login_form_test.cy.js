2ba9751ac3b711c9fc34714b901a32e6e64f342a
serpila <serpilavdar@gmail.com>
2026-03-26 23:35:28 +0300

    güncelleme

---
+++ cypress/e2e/login_form_test.cy.js
@@ -0,0 +1,33 @@
+describe('login formu testi', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:5173/')
+  })
+
+
+it("Form doğru bilgilerle doldurulduğunda submit edebiliyor", () =>{
+  cy.get("#exampleEmail").type("Cristal.Greenfelder20@gmail.com");
+  cy.get("#examplePassword").type("Y3sYRjuuDmJgooq");
+  cy.get("#terms").check();
+  cy.get('button[type="submit"]').should("not.be.disabled");
+  cy.get('button[type="submit"]').click();
+  cy.url().should("include","/success");
+})
+
+it("Email yanlış girildiğinde hata mesajı görünür ve buton disabled kalır", () =>{
+  cy.get("#exampleEmail").type("abcmail.com");
+  cy.get('[data-cy="email-error"]').should("exist");
+  cy.contains("Please enter a valid email address").should("be.visible");
+  cy.get('button[type="submit"]').should("be.disabled");
+})
+
+it("Email ve password yanlış girildiğinde hata mesajları görünür ve buton disabled kalır", () =>{
+  cy.get("#exampleEmail").type("abcmail.com");
+  cy.get("#examplePassword").type("123");
+  cy.get('[data-cy="email-error"]').should("exist");
+  cy.contains("Please enter a valid email address").should("be.visible");
+  cy.get('[data-cy="password-error"]').should("exist");
+  cy.contains("Password must be at least 4 characters long").should("be.visible");
+  cy.get('button[type="submit"]').should("be.disabled");
+})
+
+})
---
+++ src/components/Success.jsx
@@ -0,0 +1,13 @@
+function Success() {
+
+  return (
+    
+    <div className="text-center p-5">
+      <h2>🎉 Giriş Başarılı!</h2>
+      <p>Hoş geldiniz.</p>
+    </div>
+    
+  );
+}
+
+export default Success;
